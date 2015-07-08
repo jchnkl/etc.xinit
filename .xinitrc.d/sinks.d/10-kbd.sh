2f51ed8d8874c8071071f2c720836080366f18b9
Jochen Keil <jochen.keil@gmail.com>
2015-07-08 20:55:29 +0200

    Configure second Noopoo kbd too

--- .xinitrc.d/sinks.d/10-kbd.sh
+++ .xinitrc.d/sinks.d/10-kbd.sh
@@ -2,13 +2,24 @@
 
 case $@ in
   UDEV*add*/devices/*/input/*\ *)
-    NAME="HID 1096:0284"
+    Noppoo1="HID 1097:0284"
+    Noppoo2="HID 1007:0600"
+    ThinkPad="Lite-On Technology Corp. ThinkPad USB Keyboard with TrackPoint"
     HIDRAW=$(echo $@ | cut -d' ' -f 4)
 
     if [ -f "/sys/${HIDRAW}/name" ]; then
-      if [ "$(cat /sys/${HIDRAW}/name)" == "${NAME}" ]; then
+      if [ "$(cat /sys/${HIDRAW}/name)" == "${Noppoo1}" ]; then
         sleep 0.5
-        echo "xkbcomp for ${NAME}"
+        echo "Configuring ${Noppoo1} .."
+        xkbcomp -I${HOME}/.xkb ${HOME}/.xkb/keymap/default $DISPLAY
+      elif [ "$(cat /sys/${HIDRAW}/name)" == "${Noppoo2}" ]; then
+        sleep 0.5
+        echo "Configuring ${Noppoo2} .."
+        xkbcomp -I${HOME}/.xkb ${HOME}/.xkb/keymap/default $DISPLAY
+      elif [ "$(cat /sys/${HIDRAW}/name)" == "${ThinkPad}" ]; then
+        sleep 0.5
+        echo "Configuring ${ThinkPad} .."
+        setxkbmap -option 'caps:swapescape'
         xkbcomp -I${HOME}/.xkb ${HOME}/.xkb/keymap/default $DISPLAY
       fi
     fi
